20b22e36041a480d61419e04a9c5b9219a232a1b
ttyizm00 <38588229+tetsuya-izumi@users.noreply.github.com>
2022-03-04 12:45:59 +0900

    Add files via upload

--- memo/script.js
+++ memo/script.js
@@ -1,5 +1,7 @@
+// 要素を取得する
 const addBtn = document.getElementById('add')
 
+// 作成ボタンのクリックイベントの登録
 addBtn.addEventListener('click', () => addNewNote())
 
 function addNewNote(text = '') {
@@ -7,7 +9,7 @@ function addNewNote(text = '') {
     const note = document.createElement('div')
     // noteクラスを追加
     note.classList.add('note')
- 
+
     // メモ帳を追加
     note.innerHTML = `
     <div class="tools">
@@ -17,23 +19,26 @@ function addNewNote(text = '') {
     <div class="main ${text ? "" : "hidden"}"></div>
     <textarea class="${text ? "hidden" : ""}"></textarea>
     `
- 
     // bodyの子要素として追加
     document.body.appendChild(note)
-}
 
-
-function addNewNote(text = '') {
-    // div要素を作成
-    // noteクラスを追加
- 
-    // メモ帳を追加
-    ...
- 
     // 操作に必要な要素を取得
     const editBtn = note.querySelector('.edit')
     const deleteBtn = note.querySelector('.delete')
     const main = note.querySelector('.main')
     const textArea = note.querySelector('textarea')
- 
+
+    // 削除のクリックイベントの登録
+    deleteBtn.addEventListener('click', () => {
+        deleteNote(note)
+    })
+    // メモ帳削除
+function deleteNote(note) {
+    // ノートを削除
+    note.remove()
+  
+    // ローカルストレージの更新
+    updateLS()
+  }
 }
+
